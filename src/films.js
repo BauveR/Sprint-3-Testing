6664de72848c36109d7f6fa00a6ccdca7f98aefe
BauveR <rick.bauve@gmail.com>
2025-05-29 12:41:54 +0200

    get all directors using .map to return a new array with selected director

--- src/films.js
+++ src/films.js
@@ -1,7 +1,8 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
 
-  
+return array.map((movie) => movie.director);
+
 }
 
 // Exercise 2: Get the films of a certain director
